e3d8d824c6749bd82d3ef2d9833d64707a080e45
Karpatka2238 <101676223+Karpatka2238@users.noreply.github.com>
2022-05-09 22:23:28 +0200

    utworzenie klasy infografika i testinfografika

---
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/lab4a/Infografika.java
@@ -0,0 +1,69 @@
+package pl.lublin.wsei.java.cwiczenia;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Infografika {
+    public String tytul;
+    public String adres_strony;
+    public String adres_grafiki;
+    public String adres_miniaturki;
+    public String szerokosc;
+    public String wysokosc;
+
+    public void print(){
+        System.out.printf("\n\nTytul:%s \n",tytul);
+        System.out.printf("Adres strony:%s \n",adres_strony);
+        System.out.printf("Adres grafiki:%s\n",adres_grafiki);
+        System.out.printf("Adres miniaturki:%s\n",adres_miniaturki);
+        System.out.printf("Rozmiar :%s x %s \n\n",szerokosc,wysokosc);
+
+    }
+    public Infografika(String tekst){
+        Pattern pat = Pattern.compile("<title><!\\[CDATA\\[(.*)\\]\\]");
+        Matcher m = pat.matcher(tekst);
+        if(m.find())
+            tytul=m.group(1);
+        else
+            tytul="asd";
+
+        pat = Pattern.compile("<link>(.*)</link>");
+        m = pat.matcher(tekst);
+        if(m.find())
+            adres_strony=m.group(1);
+        else
+            adres_strony="";
+
+
+
+        pat = Pattern.compile("width=\"(.*)\"\\s*height");
+        m = pat.matcher(tekst);
+        if(m.find())
+            szerokosc=m.group(1);
+        else
+            szerokosc= "0";
+
+        pat = Pattern.compile("height=\"(.*)\"");
+        m = pat.matcher(tekst);
+        if(m.find())
+            wysokosc=m.group(1);
+        else
+            wysokosc= "0";
+
+        pat = Pattern.compile("<media:content\\s*url=\"(.*)\"\\s*type");
+        m = pat.matcher(tekst);
+        if(m.find())
+            adres_grafiki=m.group(1);
+        else
+            adres_grafiki= "0";
+
+        pat = Pattern.compile("<media:thumbnail\\s*url=\"(.*)\"");
+        m = pat.matcher(tekst);
+        if(m.find())
+            adres_miniaturki=m.group(1);
+        else
+            adres_miniaturki= "0";
+
+    }
+
+}
---
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/lab4a/test/TestRegEx.java
@@ -0,0 +1,26 @@
+package pl.lublin.wsei.java.cwiczenia.lab4a.test;
+
+public class TestRegEx {
+
+    public static void main(String[] args) {
+        String exitem = "<item>\n" +
+                "<title>\n" +
+                "<![CDATA[ Infografika - Koniunktura gospodarcza w kwietniu 2022 r. ]]>\n" +
+                "</title>\n" +
+                "<pubDate>\n" +
+                "<![CDATA[ Fri, 22 Apr 2022 13:30:00 +0200 ]]>\n" +
+                "</pubDate>\n" +
+                "<link>https://stat.gov.pl/infografiki-widzety/infografiki/infografika-koniunktura-gospodarcza-w-kwietniu-2022-r-,38,70.html</link>\n" +
+                "<guid isPermaLink=\"false\">https://stat.gov.pl/infografiki-widzety/infografiki/infografika-koniunktura-gospodarcza-w-kwietniu-2022-r-,38,70.html</guid>\n" +
+                "<media:content url=\"https://stat.gov.pl/files/gfx/portalinformacyjny/pl/defaultaktualnosci/5866/38/70/1/infografika_koniunktura_gospodarcza_03_2022.png\" type=\"image/png\" width=\"2362\" height=\"4548\">\n" +
+                "<media:description type=\"plain\">\n" +
+                "<![CDATA[ ]]>\n" +
+                "</media:description>\n" +
+                "<media:thumbnail url=\"https://stat.gov.pl//gfx/portalinformacyjny/_thumbs/pl/defaultaktualnosci/5866/38/70/1/infografika_koniunktura_gospodarcza_03_2022,k1uUwl-caFOE6tCTiHtf.png\"/>\n" +
+                "</media:content>\n" +
+                "<description>\n" +
+                "<![CDATA[ <div><img src=\"https://stat.gov.pl//gfx/portalinformacyjny/_thumbs/pl/defaultaktualnosci/5866/38/70/1/infografika_koniunktura_gospodarcza_03_2022,k1uUwl-caFOE6tCTiHtf.png\" alt=\"\" width=\"280\" height=\"212\"/></div> ]]>\n" +
+                "</description>\n" +
+                "</item>";
+    }
+}
